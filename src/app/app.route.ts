d553e0ede1a4e7aa76d3fc8e3e06deb5c26f1bc2
Shengyi Zhou <leolionly5@outlook.com>
2021-01-03 23:18:05 +0100

    commit

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -2,10 +2,5 @@ import { AdpageComponent } from './components/adpage/adpage.component';
 
 
 export const appRoutes=[
-    {path : 'login', component : AdpageComponent},
-    {path : 'movielist', component : AdpageComponent},
-    {path : 'playmovie', component : AdpageComponent},
-    {path : 'ratemovie', component : AdpageComponent},
-    {path : 'welcome', component : AdpageComponent},
-    {path : 'state', component : AdpageComponent}
+    {path : '', component : AdpageComponent},
 ]
